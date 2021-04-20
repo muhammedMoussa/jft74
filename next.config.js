e4324b83a7ec8a133da279f0bdcfd4c6391697e2
muhammedMoussa <muhammedmoussa@hotmail.com>
2021-04-20 21:52:14 +0200

    update assets

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = {
-  assetPrefix: ghPages ? '/jft74.github.io/' : ''   // customize this value
+  //   assetPrefix: ghPages ? '/jft74.github.io/' : ''   // customize this value,
+  assetPrefix: "/jft74/",
+  env: {
+    BACKEND_URL: "/jft74",
+  },
+  basePath: "/jft74",
 };
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href={`${process.env.BACKEND_URL}/favicon.ico`} />
       </Head>
 
       <main className={styles.main}>
