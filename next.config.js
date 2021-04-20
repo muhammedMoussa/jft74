f05ca6000c7d4b9f6dea935387dc687979381435
muhammedMoussa <muhammedmoussa@hotmail.com>
2021-04-20 22:22:51 +0200

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = {
-  assetPrefix: ghPages ? '/jft74/' : '',   // customize this value,
-  // assetPrefix: "/jft74/",
+  assetPrefix:
+    ghPages && process.env.NODE_ENV === "production" ? "/jft74/" : "",
   env: {
     BACKEND_URL: "/jft74",
   },
-  basePath: "/jft74",
+  basePath: ghPages && process.env.NODE_ENV === "production" ? "/jft74/" : "",
 };
